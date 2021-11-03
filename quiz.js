1f3cf9999bf6476ef348a6573b48e0cca1a68d1d
= <=>
2021-11-03 21:33:57 +0700

    Check out

---
+++ quiz.js
@@ -0,0 +1,306 @@
+const arr1 = [
+  "Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  "Have you ever gotten arrested?",
+  "Have you ever made out with a complete stranger for asking his or her name?",
+  "Have you ever skipped school to do something more fun and gotten caught?",
+  "Have you ever gotten scared about flying on a plane?",
+  "Have you ever gotten called out at school because you were wearing something embarrassing?",
+  "Have you ever done something so bad at school that you got suspended for it?",
+  "Have you ever been involved in a car accident that was your fault?",
+  "Have you ever tried to pole dance at a club before?",
+  "Have you ever been so upset about something that happened that day that you cried yourself to sleep that night?",
+];
+const arr2 = [
+  "Have you ever gotten lost somewhere that you have spent significant amounts of time in the past?",
+  "Have you ever tried to sing karaoke at a bar or a club somewhere?",
+  "Has someone ever commented on how loudly you sing in the shower?",
+  "Have you ever glue your hand or finger to something during an arts and crafts project?",
+  "Have you ever gone skinny-dipping before?",
+  "Have you ever gone more than a week without taking a shower?",
+  "Have you ever gone to school wearing dirty clothes because you didn't have any clean clothes to wear?",
+  "Have you ever gotten caught cheating on a test?",
+  "Have you ever forgotten someone's name even though you’ve spent time with them in the past?",
+  "Have you ever drank so much that you ended up blacking out?",
+  "Have you ever gotten caught playing prank on one of your friends?",
+];
+
+const arr3 = [
+  "Have you ever failed a class before?",
+  "Have you ever run away from home because you were so upset at your parents?",
+  "Have you ever made one of your parents cry in the past?",
+  "Have you ever dated someone more than once even though you knew going back to them was bad for you?",
+  "Have you shot a gun at some point in your life?",
+  "Have you ever gone to a dance alone because you couldn't get anyone to go with you?",
+  "Have you ever broken a bone in the past?",
+  "Have you ever gotten locked out of your own house?",
+  "Have you ever laughed so hard that something you were drinking came out of your nose?",
+  "Have you ever lied about your age to get into a bar or a club?",
+];
+const arr = [
+  {
+    id: "1a",
+    content: "1a:Have you ever failed a class before?",
+  },
+  {
+    id: "1b",
+    content:
+      "1b:Have you ever run away from home because you were so upset at your parents?",
+  },
+  {
+    id: "1c",
+    content:
+      "1c: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1aa",
+    content:
+      "1aa: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1ab",
+    content: "1ab: Have you shot a gun at some point in your life?",
+  },
+  {
+    id: "1ac",
+    content:
+      "1ac: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1ba",
+    content:
+      "1ba: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1bb",
+    content:
+      "1bb: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1bc",
+    content:
+      "1ac: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1ca",
+    content:
+      "1ca: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1cb",
+    content:
+      "1cb: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1cc",
+    content:
+      "1ac: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1aaa",
+    content:
+      "1aaa: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1aab",
+    content:
+      "1aab: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1aac",
+    content:
+      "1aac: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1aba",
+    content:
+      "1aba: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1abb",
+    content:
+      "1abb: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1abc",
+    content:
+      "1abc: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1aca",
+    content:
+      "1aca: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1acb",
+    content:
+      "1acb: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1acc",
+    content:
+      "1acc: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1baa",
+    content:
+      "1baa: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1bab",
+    content:
+      "1bab: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1bac",
+    content:
+      "1bac: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1bba",
+    content:
+      "1bba: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1bbb",
+    content:
+      "1bbb: .Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1bbc",
+    content:
+      "1bbc: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1bca",
+    content:
+      "1aca: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1bcb",
+    content:
+      "1acb: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1bcc",
+    content:
+      "1acc: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1caa",
+    content:
+      "1caa: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1cab",
+    content:
+      "1cab: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1cac",
+    content:
+      "1cac: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1cba",
+    content:
+      "1cba: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1cbb",
+    content:
+      "1cbb: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1cbc",
+    content:
+      "1cbc: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1cca",
+    content:
+      "1cca: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1ccb",
+    content:
+      "1ccb: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+  {
+    id: "1ccc",
+    content:
+      "1ccc: Have you ever had strong feelings for someone who didn't have the same feelings as you?",
+  },
+];
+function $(name) {
+  return document.querySelector(name);
+}
+
+//Selector
+const question = $("#question");
+const yesBtn = $("#yes-btn");
+const noBtn = $("#no-btn");
+const noIdeaBtn = $("#noIdea-btn");
+const timeRemaining = $("#time-remaining");
+const timeAnswer = $("#time-answer");
+
+//Functions
+window.onload = function () {
+  question.innerHTML = `Welcome to "SQUIZ GAME"! You only have 100 seconds to complete all the questions, if not, you are done for! Understand?`;
+  question.style.color = "red";
+};
+
+//Timing functions
+var count1 = 100;
+var counter1 = setInterval(remaining, 1000);
+function remaining() {
+  count1 = count1 - 1;
+  timeRemaining.innerHTML = count1;
+  if (count1 <= 0) {
+    clearInterval(counter1);
+    loseTheGame();
+  }
+}
+
+var count2 = 10;
+var counter2 = setInterval(rush, 1000);
+function rush() {
+  count2 = count2 - 1;
+  timeAnswer.innerHTML = count2;
+  if (count2 <= 0) {
+    clearInterval(counter2);
+    loseTheGame();
+  }
+}
+
+function loseTheGame() {
+  alert("you're done for");
+  clearInterval(counter2);
+  clearInterval(counter1);
+}
+
+//Function with button
+function answerQuestion(option) {
+  const questionId = document
+    .getElementById("question")
+    .getAttribute("questionId");
+  console.log("ques id", questionId);
+  let idNextQuestion = questionId + option;
+  if (idNextQuestion.length > 4) {
+    if (window.confirm("Run out of questions, do you want to restart")) {
+      idNextQuestion = "1a";
+    } else {
+      return;
+    }
+  }
+  console.log("idques", idNextQuestion);
+  const contentNextQuestion = arr.filter(
+    (item) => item.id === idNextQuestion
+  )[0].content;
+  question.innerHTML = contentNextQuestion;
+  question.setAttribute("questionId", idNextQuestion);
+  count2 = 10;
+  rush();
+}
+yesBtn.onclick = () => answerQuestion("a");
+noBtn.onclick = () => answerQuestion("b");
+noIdeaBtn.onclick = () => answerQuestion("c");
